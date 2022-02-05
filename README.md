25c4a87223549b30e705aeb9cd068312a77a9f65
George Jensen <63027688+george-jensen@users.noreply.github.com>
2022-02-05 15:11:31 +1300

    Added 'record-all' param to record all mouse movement

--- README.md
+++ README.md
@@ -10,7 +10,7 @@ This allows you to record keyboard and mouse input, and play it back (with loopi
 
 <h1>Usage</h1>
 
-- Simply run the 'record.py' file with Python3 using one input argument -> "name_of_recording". Whatever you do thereafter will be recorded.
+- Simply run the 'record.py' file with Python3 using an input argument -> "name_of_recording" and a second optional argument 'record-all' - this will record all mouse movement, as well as clicks and keyboard. If your recording is very lenghty than I'd advise avoiding the optional parameter, and the program will only record the mouse movement for dragging (clicks and their location will still be recorded). Everything after this command will be recorded.
 - To end the recording click the escape key (to end the Keyboard thread), and hold down the right click on the mouse for more than 2 seconds - then release (to end the Mouse thread).
 - To play back the recording, run the 'play.py' file with Python3 using two input arguments -> "name_of_recording" 'number_of_repeats'.
 
@@ -18,13 +18,11 @@ If you want to kill the play-back early, you can trigger a KeyboardInterrupt by 
 
 <h2>Notes</h2>
 
-- The data file takes in mouse movements every 50th of a second when holding down the left click on the mouse (dragging). If you aren't dragging a lot then you can run a relatively lengthy automation without creating a huge data file, however, if you're dragging a lot, keep the recording relatively short, as the data file will quickly fill up with mouse-movement JSON data.
-
-Scrolling functionality is supported, however it is not completely precise, due to the carry through nature of scrolling on many applications. So it is recommended to drag scrollbars when available.
+Scrolling functionality is supported, however it is not completely precise, due to the carry through nature of scrolling on many applications. So it is recommended to drag scrollbars when available. If you find you need to scroll to the top or bottom of pages then try and over-compensate when recording before continuing.
 
 <h2>Compatablitity</h2>
 
-This is Mac compatiable – as I haven't been able to find others out there that are.
+This is Mac compatiable – as I haven't been able to find others out there that are.
 
 <h2>Authors</h2>
 George Jensen
--- record.py
+++ record.py
@@ -7,15 +7,22 @@ import sys
 n = len(sys.argv)
 
 if n < 2:
-    exit("Takes an argument - name of recording")
+    exit("Takes a compulsory argument - name of recording, and optional argument - record-all")
 
-if n > 2:
-    exit("Only takes one argument - name of recording")
+if n > 3:
+    exit("Only takes two arguments - name of recording and (optional) record-all")
 
 if n == 2:
     name_of_recording = str(sys.argv[1])
+    record_all = False
+if n == 3:
+    if str(sys.argv[2]) != "record-all":
+        exit("The second argument given must be 'record-all', otherwise only pass the name of recording as a parameter")
+    name_of_recording = str(sys.argv[1])
+    record_all = True
 
 print("Hold right click for more than 2 seconds (and then release) to end the recording for mouse and click 'esc' to end the recording for keyboard (both are needed to finish recording)")
+
 storage = []
 count = 0
 
@@ -37,10 +44,22 @@ def on_release(key):
         
 
 def on_move(x, y):
-    if len(storage) >= 1:
-        if (storage[-1]['action'] == "pressed" and storage[-1]['button'] == 'Button.left') or (storage[-1]['action'] == "moved" and time.time() - storage[-1]['_time'] > 0.02):
+    if (record_all) == True:
+        if len(storage) >= 1:
+            if storage[-1]['action'] != "moved":
+                json_object = {'action':'moved', 'x':x, 'y':y, '_time':time.time()}
+                storage.append(json_object)
+            elif storage[-1]['action'] == "moved" and time.time() - storage[-1]['_time'] > 0.02:
+                json_object = {'action':'moved', 'x':x, 'y':y, '_time':time.time()}
+                storage.append(json_object)
+        else:
             json_object = {'action':'moved', 'x':x, 'y':y, '_time':time.time()}
             storage.append(json_object)
+    else:
+        if len(storage) >= 1:
+            if (storage[-1]['action'] == "pressed" and storage[-1]['button'] == 'Button.left') or (storage[-1]['action'] == "moved" and time.time() - storage[-1]['_time'] > 0.02):
+                json_object = {'action':'moved', 'x':x, 'y':y, '_time':time.time()}
+                storage.append(json_object)
 
 def on_click(x, y, button, pressed):
     json_object = {'action':'pressed' if pressed else 'released', 'button':str(button), 'x':x, 'y':y, '_time':time.time()}
